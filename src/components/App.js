a630743fa5c48b9c2ea3da425317c8ee5f9127bd
Tim <timtran007@gmail.com>
2022-10-20 22:09:38 -0500

    commit tree a591835d98bdb0200dc59bd411e1f0323a9ca658

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar.js"
+// import Home from "./Home.js"
+// import About from "./About.js"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* <Home /> */}
+      {/* <About /> */}
     </div>
   );
 }
